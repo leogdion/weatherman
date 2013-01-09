7cddd71e37f39c55a53208739f027bbe5618f6fb
leogdion <leogdion@brightdigit.com>
2013-01-08 19:14:40 -0500

    added vs deps, started fix on json emumerator

--- src/weatherman/main.c
+++ src/weatherman/main.c
@@ -14,78 +14,83 @@
 
 static size_t write_data(char *ptr, size_t size, size_t nmemb, char *userdata)
 {
-    if (strlen(userdata) > 0) {
-        strncat(userdata, ptr, nmemb);
-    } else {
-        strcpy(userdata, ptr);
-    }
-    return size * nmemb;
+	if (strlen(userdata) > 0) {
+		strncat(userdata, ptr, nmemb);
+	} else {
+		strcpy(userdata, ptr);
+	}
+	return size * nmemb;
 }
 
 static void enumerate (json_value * value)
 {
-    int index;
-    for (index = 0; index < value->u.object.length; index++) {
-        puts(value->u.object.values[index].name);
-        if (value->u.object.values[index].value->type == json_object)
-        {
-            enumerate(value->u.object.values[index].value);
-        }
-        else
-        {
-            puts(value->u.object.values[index].value->u.string.ptr);
-        }
-    }
+	int index;
+	for (index = 0; index < value->u.object.length; index++) {
+		puts(value->u.object.values[index].name);
+		switch (value->u.object.values[index].value->type) 
+		{
+		case json_object:
+			enumerate(value->u.object.values[index].value);
+			break;
+		case json_string:
+			puts(value->u.object.values[index].value->u.string.ptr);
+			break;
+		default:
+			puts("?");
+			break;
+		}
+	}
 }
 
 int main(int argc, const char * argv[])
 {
-    char * text;
-    CURL *curl;
-    CURLcode res;
-    
-    text  = (char *)malloc(sizeof(char));
-    memset(text, '\0', 4096 * sizeof(char));
-    
-    curl = curl_easy_init();
-    if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "http://api.wunderground.com/api/1128524773875b60/conditions/q/KLAN.json");
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, text);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-        
+	char * text;
+	CURL *curl;
+	CURLcode res;
+	json_value * value;
+
+	text  = (char *)malloc(sizeof(char) * 4096);
+	memset(text, '\0', 4096 * sizeof(char));
+
+	curl = curl_easy_init();
+	if(curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, "http://api.wunderground.com/api/1128524773875b60/conditions/q/KLAN.json");
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, text);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+
 #ifdef SKIP_PEER_VERIFICATION
-        /*
-         * If you want to connect to a site who isn't using a certificate that is
-         * signed by one of the certs in the CA bundle you have, you can skip the
-         * verification of the server's certificate. This makes the connection
-         * A LOT LESS SECURE.
-         *
-         * If you have a CA cert for the server stored someplace else than in the
-         * default bundle, then the CURLOPT_CAPATH option might come handy for
-         * you.
-         */
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+		/*
+		* If you want to connect to a site who isn't using a certificate that is
+		* signed by one of the certs in the CA bundle you have, you can skip the
+		* verification of the server's certificate. This makes the connection
+		* A LOT LESS SECURE.
+		*
+		* If you have a CA cert for the server stored someplace else than in the
+		* default bundle, then the CURLOPT_CAPATH option might come handy for
+		* you.
+		*/
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
 #endif
-        
+
 #ifdef SKIP_HOSTNAME_VERFICATION
-        /*
-         * If the site you're connecting to uses a different host name that what
-         * they have mentioned in their server certificate's commonName (or
-         * subjectAltName) fields, libcurl will refuse to connect. You can skip
-         * this check, but this will make the connection less secure.
-         */
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+		/*
+		* If the site you're connecting to uses a different host name that what
+		* they have mentioned in their server certificate's commonName (or
+		* subjectAltName) fields, libcurl will refuse to connect. You can skip
+		* this check, but this will make the connection less secure.
+		*/
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
 #endif
-        res = curl_easy_perform(curl);
-        json_value * value = json_parse(text);
-        
-        enumerate(value);
-        /* always cleanup */
-        curl_easy_cleanup(curl);
-        free(text);
-        free(value);
-        
-    }
-    return 0;
+		res = curl_easy_perform(curl);
+		value = json_parse(text);
+
+		enumerate(value);
+		/* always cleanup */
+		curl_easy_cleanup(curl);
+		free(text);
+		free(value);
+
+	}
+	return 0;
 }
 
